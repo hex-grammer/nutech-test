b7dd65ebb1d0511a424cb54adbc7625fec7feeef
Rizal Iswandy <=>
2023-07-28 15:29:03 +0800

    setup project

---
+++ TODO.md
@@ -0,0 +1,21 @@
+# Project
+
+Project Description
+
+<em>[TODO.md spec & Kanban Board](https://bit.ly/3fCwKfM)</em>
+
+### Todo
+
+- [ ] Setup github  
+- [ ] layout  
+- [ ] custom component  
+- [ ] index page  
+
+### In Progress
+
+
+### Done ✓
+
+- [x] cleanup boilerplate  
+- [x] setup database  
+
---
+++ src/pages/api/barang/read.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      const barang = await prisma.barang.findMany();
+      res.status(200).json(barang);
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
